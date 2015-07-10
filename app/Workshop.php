3147723064b554123bb85138e95ad08b311048b2
Kevin Saunders <ksaunders@saunderswebsolutions.com>
2015-07-10 16:24:34 +0100

    Workshop listener update

--- app/Workshop.php
+++ app/Workshop.php
@@ -13,6 +13,10 @@ class Workshop extends Model
      */
     protected $table = 'mysite_workshop';
 
+    /**
+     * Gets the workshop date formatted as Thursday 29th Oct 2015 at 19:30
+     * @return string
+     */
     public function getWorkshopDate()
     {
         return date('l jS M Y \a\t H:i',strtotime($this->workshop_date));
