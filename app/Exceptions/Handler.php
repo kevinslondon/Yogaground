70e985e32a3e039561504811da3dead709bc0f49
Kevin Saunders <ksaunders@saunderswebsolutions.com>
2015-07-13 21:34:28 +0100

    Error pages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -38,6 +39,19 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        // 404 page when a model is not found
+        if ($e instanceof ModelNotFoundException) {
+            return response()->view('errors.404', [], 404);
+        }
+
+        if ($this->isHttpException($e)) {
+            return $this->renderHttpException($e);
+        } else {
+            // Custom error 500 view on production
+            if (app()->environment() == 'production') {
+                return response()->view('errors.500', [], 500);
+            }
+            return parent::render($request, $e);
+        }
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+
+@section('content')
+    <h1>Page Missing</h1>
+    Sorry, that page is not on the site, please check the menu and try again.
+
+@endsection
