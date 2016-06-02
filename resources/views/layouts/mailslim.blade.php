d9b27cfd989c546e7289e7ec25faac639f2caa8a
Kevin Saunders <ksaunders@saunderswebsolutions.com>
2016-06-02 19:07:23 +0100

    Update to mail chimp signup on right bar

--- resources/views/layouts/mailslim.blade.php
+++ resources/views/layouts/mailslim.blade.php
@@ -8,7 +8,7 @@
 <div id="mc_embed_signup">
     <form action="//yogaground.us9.list-manage.com/subscribe/post?u={{$mail_chimp_u}}&amp;id={{$mail_chimp_id}}" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
-            <label for="mce-EMAIL">Newsletter <signup></signup></label>
+            <label for="mce-EMAIL">Get News, Articles & Tips <signup></signup></label>
             <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
             <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cf7504563a797f6fcfc935c15_c47245e2ed" tabindex="-1" value=""></div>
