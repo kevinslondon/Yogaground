c5ded7568f8230bf102c72435501b890a5cba3f2
Kevin Saunders <ksaunders@saunderswebsolutions.com>
2017-02-06 20:09:25 +0000

    adding workshop additions

--- resources/views/workshops.blade.php
+++ resources/views/workshops.blade.php
@@ -8,6 +8,10 @@
 
     @foreach($workshops as $workshop)
         <h2 class="workshop_link"><a href="/workshop/{{$workshop->id}}" >{{$workshop->name}} </a> </h2>
+        @if($workshop->image)
+        <img src="{{$workshop->image}}" alt="" align="left" style="margin-right:15px;"
+             class="image_border">
+        @endif
             {!!$workshop->description  !!}
 
     @endforeach
