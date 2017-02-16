ce8c796d49dc92ae0a63a2e922129db8d4464264
Kevin Saunders <ksaunders@saunderswebsolutions.com>
2017-02-16 15:13:23 +0000

    UI updates

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>North London Yoga,N4, Yoga Ground, UK: Kevin Saunders</title>
-    <link href="/css/yoga_style.css" rel="stylesheet" media="(orientation: landscape)" type="text/css"/>
-    <link href="/css/mobile_portrait.css" rel="stylesheet" media="(orientation: portrait)" type="text/css"/>
+    <link href="/css/yoga_style.css" rel="stylesheet" media="all" type="text/css" />
+    <link href="/css/mobile_portrait.css" rel="stylesheet" media="(orientation: portrait)" type="text/css" />
+    <link href="/css/landscape_view.css" rel="stylesheet" media="(orientation: landscape)" type="text/css" />
     <meta name="keywords"
           content="Yoga,Hatha,North, Meridian,Energy,London,Relaxation,Release,Muscle,Injury Prevention,Joint,Healing"/>
     <meta name="description" content="Hatha yoga influenced by okido yoga, Alexander technique and shiatsu."/>
@@ -13,45 +14,15 @@
 
 <body>
 
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-
 <div id="container_centre">
     <section id="container">
         <header>
-            <a href="/index.php"><img src="/images/headers.jpg" id="header_image" alt="" width="1000" height="183"
-                                      border="0"/></a>
+            <a href="/"></a>
         </header>
 
         <aside id="side_bar"><img src="/images/left_spacer.jpg" alt="" width="75" height="383"/></aside>
-        <nav id="left_bar">
-            <h2 class="menu_link"><a href="/" title="Yoga Ground Home">Home</a></h2>
-
-            <h2 class="menu_link"><a href="/about" title="About Yoga Ground">About Yoga Ground</a></h2>
-
-            <h2 class="menu_link"><a href="/lessons" title="Lessons at Yoga Ground">Lessons</a></h2>
-
-            <h2 class="menu_link"><a href="/workshops" title="Workshops at Yoga Ground">Workshops</a></h2>
-
-            <h2 class="menu_link"><a href="/yoga_one_to_one" title="One to one yoga lessons at yoga ground">One to
-                    ones</a></h2>
-
-            <h2 class="menu_link"><a href="/contact" title="Contact Yoga Ground">Contact Yoga Ground</a></h2>
-
-            <h2 class="menu_link"><a href="/reviews" title="Testimonials / Reviews">Testimonials</a></h2>
-
-            <h2 class="menu_link"><a href="/blog/" title="Yoga Ground Blog">Blog</a></h2>
-
-            <p id="menu_bottom_separator"><img src="/images/greenline.jpg" alt="" width="194" height="17"/></p>
-
-            <p id="menu_bottom_image"><img src="/images/left/3.jpg" alt="" width="194" height="323"/></p>
+        <nav >
+            @include('layouts.menu')
         </nav>
 
         <article id="content">
@@ -69,25 +40,14 @@
 
             <p><img src="/images/right_plant_motif.jpg" alt="" width="191" height="59" class="mobile_hide"/></p>
 
-
-            <p><br/>
-                <img src="/images/right_collage.jpg" alt="" width="190" height="155"
-                     class="image_left right_bar_break"/>
-                <img src="/images/yoga_alliance_200_teacher.jpg" alt="" width="190" height="108"
-                     class="image_left right_bar_break"/>
-                <img src="/images/relax-and-renew-badge.jpg" alt="Relax and restore trainer" width="190" height="190"
-                     class="image_left right_bar_break"/>
-                <img src="/images/registered_exercise_prof.jpg" alt="" width="140" height="140"
-                     style="margin-left:25px;" class="image_left right_bar_break"/>
-            </p>
         </aside>
-        <aside id="side_bar_right"><img src="/images/right_spacer.jpg" alt="" width="76" height="396"/></aside>
+        <aside id="side_bar_right"><img src="/images/right_spacer.jpg" alt="" width="75" height="396"/></aside>
 
     </section>
     <br clear="left"/>
     <footer>
         Contact me: 07815 797 645&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy; Kevin Saunders
-        2015.&nbsp;&nbsp;
+        2017.&nbsp;&nbsp;
     </footer>
 </div>
 <script type="text/javascript">
